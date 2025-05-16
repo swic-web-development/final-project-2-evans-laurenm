0e47ffb7a45744707023e28714ce607b1b868ede
Lauren Evans <lauren.evans@student.swic.edu>
2025-05-16 04:22:54 -0500

    Create an actions file that fetches the dad jokes and catches errors if there are any.

---
+++ src/actions.jsx
@@ -0,0 +1,25 @@
+import store from './store'
+
+export async function fetchJokes() {
+  store.setState({ isLoading: true, error: null })
+
+  try {
+    const jokeResponse = await fetch('https://icanhazdadjoke.com/', {
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+    const jokeData = await jokeResponse.json()
+
+    store.setState({
+      joke: jokeData.joke, // fixed typo, extracted just the joke string
+      isLoading: false,
+    })
+  } catch (error) {
+    store.setState({
+      error: error.message || 'Failed to make you smile.',
+      isLoading: false,
+      joke: '',
+    })
+  }
+}
